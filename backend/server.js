6aa944d0fcf27f9f9641486e5e72655528b9464f
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2026-05-04 23:24:59 +0530

    Album Controller API created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongoDb.js';
 import connectCloudinary from './src/config/cloudinary.js';
+import albumRouter from './src/routes/albumRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/songs", songRouter);
+app.use("/api/albums", albumRouter);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
